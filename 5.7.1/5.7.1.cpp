89c0bbe0244413b80251dd0249a08c7989d10a4f
Mukhriddin Badriev <muhriddinbadriyev7@gmail.com>
2022-11-12 00:01:43 +0300

    unique vector

---
+++ 5.7.1/5.7.1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    std::vector<int> v = { 1,1,2,5,6,1,2,4 };
+
+    std::cout << "[IN]: ";
+    for (const auto& i : v) {
+        std::cout << i << ' ';
+    }std::cout << '\n';
+
+    std::sort(v.begin(), v.end()); 
+    auto it = std::unique(v.begin(), v.end());
+    v.erase(it, v.end());
+
+    std::cout << "[OUT]: ";
+    for (const auto& i : v) {
+        std::cout << i << ' ';
+    }
+    return 0;
+}
